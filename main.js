10d4ebc6b5d5c17c0a7b02c0302977ede23d9df0
Saber Chkiri <75795312+saberchkiri360@users.noreply.github.com>
2023-02-19 02:20:17 +0100

    new

--- main.js
+++ main.js
@@ -5,17 +5,16 @@ const button = document.querySelector('.button')
 let to_do = [];
 
 
-function click () { button.addEventListener('click', ()=> {
-    adding_tasks();
-})
-};
+function click() {
+    button.addEventListener('click', () => {
+      adding_tasks();
+    });
+  }
+  click();
 
-
-
-function adding_tasks () {
+  function adding_tasks() {
     let input_value = inputs.value;
-    to_do.push(input_value)
-    alert(input_value)
-}
-console.log(adding_tasks());
-console.log(to_do);
+    to_do.push(input_value);
+    alert(input_value);
+    console.log(to_do);
+  }
